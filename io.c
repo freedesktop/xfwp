f30ecc27ecc583991157f04906f61ba774d14567
Egbert Eich <eich@suse.de>
2004-02-26 09:24:07 +0000

    Importing vendor version xf86-4_3_99_903 on Wed Feb 26 01:21:00 PST 2004

--- io.c
+++ io.c
@@ -29,7 +29,7 @@ authorization from The Open Group.
 X Window System is a trademark of The Open Group.
 
 */
-/* $XFree86: xc/programs/xfwp/io.c,v 1.11 2001/12/14 20:01:43 dawes Exp $ */
+/* $XFree86$ */
 
 #include <stdio.h>
 #include <stdlib.h>
